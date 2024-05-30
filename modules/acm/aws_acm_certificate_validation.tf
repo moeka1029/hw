e2a8bbd0bddc77a2c3b29bced3ec6e65b6219fcf
Moeka Tazaki <MOEKA.TAZAKI@ibm.com>
2024-05-30 13:58:18 +0900

    output

--- modules/acm/aws_acm_certificate_validation.tf
+++ modules/acm/aws_acm_certificate_validation.tf
@@ -1,4 +1,5 @@
-resource "aws_acm_certificate_validation" "example" {
-  certificate_arn         = aws_acm_certificate.example.arn
+resource "aws_acm_certificate_validation" "aws" {
+  certificate_arn         = aws_acm_certificate.aws.arn
+  validation_record_fqdns = [for record in var.route53.cert_record : record.fqdn]
 
 }
