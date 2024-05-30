54eafe753bdfd8215714b517b8f9812d5c6e324b
Moeka Tazaki <MOEKA.TAZAKI@ibm.com>
2024-05-30 13:45:00 +0900

    output

--- output.tf
+++ output.tf
@@ -1,11 +1,8 @@
 output "domain" {
   value = {
-    domain_name = var.domain_name
-
-    zone_id = module.route53.aws.zone.zone_id
-
-    certificate_arn = module.acm.aws.certificate.arn
-    
+    domain_name = var.domain_name,
+    zone_id = module.route53.aws.zone.zone_id,
+    certificate_arn = module.acm.aws.certificate.arn,
     certificate_global_arn = module.acm.aws.certificate=global.arn
   }
 }
