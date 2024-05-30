97845c8411fb010d1a6375195f5455dede8e7ba6
Moeka Tazaki <MOEKA.TAZAKI@ibm.com>
2024-05-30 14:13:23 +0900

    main

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ module "route53" {
   source = "./modules/route53"
 
   domain_name = var.domain_name
-  acm = modules.acm.aws
+  acm = module.acm.aws
 }
 
 module "acm" {
   source = "./modules/acm"
 
   domain_name = var.domain_name
-  route53 = modules.route53.aws
+  route53 = module.route53.aws
 }
