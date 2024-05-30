0261889859f7131d5cc7cf6f51a950f249d20b9b
Moeka Tazaki <MOEKA.TAZAKI@ibm.com>
2024-05-30 14:10:14 +0900

    redord

--- modules/route53/aws_route53_record.tf
+++ modules/route53/aws_route53_record.tf
@@ -1,10 +1,9 @@
-resource "aws_route53_record" "example" {
+resource "aws_route53_record" "aws" {
   for_each = {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+    for dvo in var.acm.certificate.domain_validation_options : dvo.domain_name => {
       name    = dvo.resource_record_name
       record  = dvo.resource_record_value
       type    = dvo.resource_record_type
-      zone_id = dvo.domain_name == "example.org" ? data.aws_route53_zone.example_org.zone_id : data.aws_route53_zone.example_com.zone_id
     }
   }
 
@@ -13,5 +12,5 @@ resource "aws_route53_record" "example" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = each.value.zone_id
+  zone_id         = aws_route53_zone.aws.zone_id
 }
